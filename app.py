cc87b068b27d38c1114c313305b16850a84c1eae
Mahek Bisht <mahekbisht6@gmail.com>
2026-04-24 01:13:12 +0530

    testing route

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # ---------------- HOME ----------------
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "WORKING ✅"
 
 
 # ---------------- CHAT ----------------
